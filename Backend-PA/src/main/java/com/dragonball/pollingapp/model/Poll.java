1640b20e6a5b60b9c957b867eda1122b17e58402
PavanBashetty <pavankumar.rb25@gmail.com>
2025-01-28 02:43:45 +0100

    lombok removed

--- Backend-PA/src/main/java/com/dragonball/pollingapp/model/Poll.java
+++ Backend-PA/src/main/java/com/dragonball/pollingapp/model/Poll.java
@@ -1,15 +1,10 @@
 package com.dragonball.pollingapp.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Poll {
 
     @Id
@@ -23,4 +18,33 @@ public class Poll {
     @ElementCollection
     private List<Long> votes = new ArrayList<>();
 
+    public Poll(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public List<Long> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Long> votes) {
+        this.votes = votes;
+    }
 }
