b5aef73458be728bf84a50c422df968c13c48855
PavanBashetty <pavankumar.rb25@gmail.com>
2025-01-28 03:15:39 +0100

    get apis for polls added

--- Backend-PA/src/main/java/com/dragonball/pollingapp/controllers/PollController.java
+++ Backend-PA/src/main/java/com/dragonball/pollingapp/controllers/PollController.java
@@ -2,10 +2,10 @@ package com.dragonball.pollingapp.controllers;
 
 import com.dragonball.pollingapp.model.Poll;
 import com.dragonball.pollingapp.services.PollService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -20,5 +20,17 @@ public class PollController {
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll){
         return pollService.createPoll(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPollById(@PathVariable Long id){
+        return pollService.getPollById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- Backend-PA/src/main/java/com/dragonball/pollingapp/services/PollService.java
+++ Backend-PA/src/main/java/com/dragonball/pollingapp/services/PollService.java
@@ -2,7 +2,11 @@ package com.dragonball.pollingapp.services;
 
 import com.dragonball.pollingapp.model.Poll;
 import com.dragonball.pollingapp.repositories.PollRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -15,5 +19,13 @@ public class PollService {
 
     public Poll createPoll(Poll poll){
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
     }
 }
